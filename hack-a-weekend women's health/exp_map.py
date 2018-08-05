97ecd90a321f2c60e86d9ab53d0783320644dba6
verascity <svsticker@gmail.com>
2018-08-05 17:01:13 -0400

    port trend functionality

--- hack-a-weekend women's health/exp_map.py
+++ hack-a-weekend women's health/exp_map.py
@@ -6,23 +6,49 @@ Created on Sun Aug  5 15:46:25 2018
 """
 
 import pandas as pd
+import numpy as np
 import plotly.plotly as py
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', -1)
 
 df = pd.read_csv('expenditurestats.csv')
 df.rename(index=str, columns={'Entity':'Country',
            'Health expenditure, public (% of GDP) (% of GDP)':'Public',
            'Health expenditure, private (% of GDP) (% of GDP)':'Private'}, inplace=True)
 
-
 def total_exp(row):
-    return row['Public'] + row['Private']
+    return round((row['Public'] + row['Private']), 2)
 
 df['Total'] = df.apply(total_exp, axis=1)
 
-print(df.head())
 
+df['Total'] = df['Total'].astype('float')
 
-'''data = [ dict(
+df = df[['Country', 'Code', 'Year', 'Total']]
+
+df = df.loc[df['Year'].isin([2002, 2014])]
+df = df.loc[df['Code'].str.len() == 3]
+
+def calc_trend(df):
+    
+    trend = []
+    
+    for country in np.unique(df['Country']):
+       first = df.loc[(df['Country'] == country) & (df['Year'] == 2002)]
+       second = df.loc[(df['Country'] == country) & (df['Year'] == 2014)]
+       trend.append(round(float(second['Total']) - float(first['Total']), 2))
+     
+    return pd.Series(trend)
+
+trend = calc_trend(df)
+df = df.loc[df['Year'] == 2014]
+df.loc[:,'Trend'] = trend.values
+print(df.head(20))
+
+'''
+data = [ dict(
         type = 'choropleth',
         locations = df['Code'],
         z = df['Trend'],
